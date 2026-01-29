0d64b395de27723a501359997716f40bd80f4594
Juan Manuel Velázquez <juanma261005@gmail.com>
2026-01-29 16:06:22 -0300

    separate most used colors

---
+++ lib/core/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color mainColor = Color(0xFF2881C5);
+const MaterialColor completedColor = Colors.green;
+const MaterialColor notCompletedColor = Colors.orange;
