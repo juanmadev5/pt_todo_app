367c5c9f9f7deba1e920836db0fcffdfb35d171f
Juan Manuel Velázquez <juanma261005@gmail.com>
2026-01-29 16:14:59 -0300

    create task_entity

---
+++ lib/domain/entities/task_entity.dart
@@ -0,0 +1,11 @@
+class TaskEntity {
+  final int id;
+  final String title;
+  final bool isCompleted;
+
+  TaskEntity({
+    required this.id,
+    required this.title,
+    required this.isCompleted,
+  });
+}
